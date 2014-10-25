9c619aad4bc02ced61aee57251428b8c1ce14bc3
qing <514306470@qq.com>
2014-10-25 18:27:49 +0800

    Initial Commit

---
+++ planeGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  planeGame
+//
+//  Created by Qingqing on 14-10-25.
+//  Copyright (c) 2014年 Qingqing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ planeGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  planeGame
+//
+//  Created by Qingqing on 14-10-25.
+//  Copyright (c) 2014年 Qingqing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
